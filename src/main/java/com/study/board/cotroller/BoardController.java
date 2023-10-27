a579d58f2a231576f6762e922cc76bcc90cdaa64
admin <tmdco12@naver.com>
2023-10-28 08:42:54 +0900

    제목,내용 출력 확인

--- src/main/java/com/study/board/cotroller/BoardController.java
+++ src/main/java/com/study/board/cotroller/BoardController.java
@@ -2,6 +2,7 @@ package com.study.board.cotroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,5 +12,14 @@ public class BoardController {
     public String boardWriteForm() {
 
         return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(String title, String content) {
+
+        System.out.println("제목 :" + title);
+        System.out.println("내용 :" + content);
+
+        return "";
     }
 }
