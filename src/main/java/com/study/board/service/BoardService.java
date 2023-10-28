7ec5d5c82e2ff8e69b139f47753415eb2b09ae48
admin <tmdco12@naver.com>
2023-10-29 05:41:35 +0900

    BoardService - 목록조회 추가

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.study.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BoardService {
@@ -15,5 +17,10 @@ public class BoardService {
 
         boardRepository.save(board); // boardRepository를 사용하여 Board 객체를 저장 (데이터베이스에 저장)
 
+    }
+
+    public List<Board> boardList() {
+
+        return boardRepository.findAll();
     }
 }
