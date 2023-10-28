998ec53ed83767fe8c1951ea732b735db672f122
admin <tmdco12@naver.com>
2023-10-28 11:09:43 +0900

    controller 의존성 주입

--- src/main/java/com/study/board/cotroller/BoardController.java
+++ src/main/java/com/study/board/cotroller/BoardController.java
@@ -1,13 +1,18 @@
 package com.study.board.cotroller;
 
 import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService; // 의존성 주입
 
     // GetMapping : 어떤 url로 접근할 것인지 지정해주는 어노테이션
     @GetMapping("/board/write") // localhost:808/board/write
@@ -19,8 +24,7 @@ public class BoardController {
     @PostMapping("/board/writepro")
     public String boardWritePro(Board board) { // Entity @Data로 인해서 변경 가능
 
-        System.out.println(board.getTitle());
-        System.out.println(board.getContent());
+        boardService.write(board); // 게시글 정보를 데이터베이스에 저장
 
         return "";
     }
