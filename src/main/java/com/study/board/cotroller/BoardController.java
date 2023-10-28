8149807c3e75210e7d51326f47811dcb68eb217c
admin <tmdco12@naver.com>
2023-10-28 09:33:02 +0900

    controller 매개변수 변경

--- src/main/java/com/study/board/cotroller/BoardController.java
+++ src/main/java/com/study/board/cotroller/BoardController.java
@@ -1,5 +1,6 @@
 package com.study.board.cotroller;
 
+import com.study.board.entity.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,17 +9,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BoardController {
 
+    // GetMapping : 어떤 url로 접근할 것인지 지정해주는 어노테이션
     @GetMapping("/board/write") // localhost:808/board/write
     public String boardWriteForm() {
 
-        return "boardwrite";
+        return "boardwrite"; // boardwrite.html 문서를 보여줌
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(String title, String content) {
+    public String boardWritePro(Board board) { // Entity @Data로 인해서 변경 가능
 
-        System.out.println("제목 :" + title);
-        System.out.println("내용 :" + content);
+        System.out.println(board.getTitle());
+        System.out.println(board.getContent());
 
         return "";
     }
