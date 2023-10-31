04fe838a505497b950ba3c6e08fa2a292645f0d4
admin <tmdco12@naver.com>
2023-10-31 16:57:42 +0900

    service - 파일업로드 추가

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -4,8 +4,11 @@ import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +17,20 @@ public class BoardService {
     private final BoardRepository boardRepository; // 의존성 주입을 통해 BoardRepository 객체 생성
 
     // 글 작성 처리
-    public void write(Board board) {
+    public void write(Board board, MultipartFile file) throws Exception { /* 작성 bord, file 저장, throws Exception 예외처리 */
+
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files"; /* 저장할 경로, "user.dir" = 절대경로 지정*/
+
+        UUID uuid = UUID.randomUUID(); /* 램덤 이름 생성 */
+
+        String fileName = uuid + "_" + file.getOriginalFilename(); /* 랜덤 이름_파일이름 */
+
+        File saveFile = new File(projectPath, fileName); /* 경로, 파일이름으로 저장 객체 생성 */
+
+        file.transferTo(saveFile); /* 업로드 파일을 새파일로 이동(복사) */
+
+        board.setFilename(fileName); /* entity <- fileName */
+        board.setFilepath("/files/" + fileName); /* 저장된 파일 경로, 이름 */
 
         boardRepository.save(board); // boardRepository를 사용하여 Board 객체를 저장 (데이터베이스에 저장)
 
